b3ad43bd2fc91883cb36407110862d4b9125a89b
SantiagoBarreiroCampos <72943433+SantiagoBarreiroCampos@users.noreply.github.com>
2021-12-02 16:37:22 +0100

    subir

---
+++ servidor.py
@@ -0,0 +1,67 @@
+import socket
+import threading
+import sys
+import pickle
+import os
+
+class Servidor():
+	def __init__(self, host=socket.gethostname(), port=22222):
+		self.clientes = []
+		self.nicknames = []
+		self.sock = socket.socket()
+		self.sock.bind((str(host), int(port)))
+		self.sock.listen(20)
+		self.sock.setblocking(False)
+
+		aceptar = threading.Thread(target=self.aceptarC)
+		procesar = threading.Thread(target=self.procesarC)
+		
+		aceptar.daemon = True
+		aceptar.start()
+
+		procesar.daemon = True
+		procesar.start()
+
+		while True:
+			msg = input('SALIR = Q\n')
+			if msg == 'Q':
+				print("**** TALOGOOO *****")
+				self.sock.close()
+				sys.exit()
+			else:
+				pass
+
+	def broadcast(self, msg, cliente):
+		for c in self.clientes:
+			try:
+				if c != cliente:
+					c.send(msg)
+			except:
+				self.clientes.remove(c)
+
+	def aceptarC(self):
+		while True:
+			try:
+				conn, addr = self.sock.accept()
+				nick = self.sock.getsockname
+				print(f"\nConexion aceptada via {conn}\n")
+				conn.setblocking(False)
+				self.clientes.append(conn)
+				self.nicknames.append(nick)
+				print(f'Lista usuarios: {self.nicknames}')
+			except:
+				pass
+
+	def procesarC(self):
+		print("Procesamiento de mensajes iniciado")
+		while True:
+			if len(self.clientes) > 0:
+				for c in self.clientes:
+					try:
+						data = c.recv(32)
+						if data:
+							self.broadcast(data,c)
+					except:
+						pass
+
+s = Servidor()
